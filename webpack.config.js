76fc5f051538d55030974eb6747bd02c177718ea
SantRV <santiagoreyes.training@gmail.com>
2022-07-31 16:01:36 +0930

    reset

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,53 @@
 const path = require('path');
-const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
     mode: 'development',
-    // Transform ES6 JS to regular JS
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules)/,
-                loader: 'babel-loader',
-                options: { presets: ['@babel/env'] }
+                test: /\.[jt]sx?$/,
+                use: ['babel-loader'],
+                exclude: /node_modules/,
             },
             {
                 test: /\.css$/,
-                use: ["style-loader", "css-loader"]
+                use: ['style-loader', 'css-loader'],
             },
             {
-                test: /\.s[ac]ss$/i,
-                use: ["style-loader", "css-loader", "sass-loader"]
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
             }
-        ]
+        ],
     },
-    resolve: { extensions: ['*', '.js', '.jsx'] },
-    output: { 
-        path: path.resolve(__dirname, 'dist/'),
-        publicPath: '/dist/',
-        filename: 'bundle.js'
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.jsx'],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './public/index.html'),
+        }),
+        new CleanWebpackPlugin(),
+    ],
     devServer: {
-        // contentBase
-        static : {
-          directory : path.join(__dirname, "public/")
-        },
+        static: path.join(__dirname, './src'),
         port: 3000,
-        // publicPath
-        devMiddleware:{
-           publicPath: "https://localhost:3000/dist/",
-        },
-        // hotOnly
-        hot: "only",
+        hot: 'only',
+        compress: true,
+        open: true,
     },
-    plugins: [new webpack.HotModuleReplacementPlugin()]
 };
